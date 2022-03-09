2ceacd4ea23b45fe2ba267b063b17767d9aa5267
Akanksha <akank612@gmail.com>
2022-03-09 16:57:28 +0530

    problem1 added

---
+++ problem1.js
@@ -0,0 +1,20 @@
+ 
+ 
+ 
+  
+  module.exports.problem1= function (inventory)
+  {
+  
+  
+  for(var i=0;i<inventory.length;i++)
+  {
+  
+  if(i==32)
+  {
+  return inventory[i];
+  }
+
+  
+  }
+  
+  }
